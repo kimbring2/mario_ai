cc4c8287e5dd6557a7f7418773814ef55dfb8a4b
kimbring2 <kimbring2@gmail.com>
2023-02-04 11:06:01 +0900

    level state and example file update

---
+++ env_test.py
@@ -0,0 +1,10 @@
+import retro
+print("retro.__file__: ", retro.__file__)
+
+env = retro.make(game='SuperMarioBros3-Nes', state='SuperMarioBros3.DarkLand.Level10')
+
+env.reset()
+
+while True:
+    _obs, _rew, done, _info = env.step(env.action_space.sample())
+    env.render()
